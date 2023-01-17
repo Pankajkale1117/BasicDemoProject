9677c1622b4284cb808c2509aa4bed7b1bb1d770
pankaj <pankajkale1117@gmail.com>
2023-01-17 22:04:50 +0530

    nothing to commit

---
+++ src/com/velocity/demo/Test.java
@@ -0,0 +1,21 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> list= new ArrayList<String>();
+		list.add("Java");
+		list.add("Hibernate");
+		list.add("Springboot");
+		System.out.println(list);
+		
+		
+	}
+
+}
